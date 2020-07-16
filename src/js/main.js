13c08aa64bd33784be142b1b50199c7ed8ed1c5d
sanyokstd <59456430+sanyokstd@users.noreply.github.com>
2020-07-16 17:34:35 +0300

    1
    выбор сектора(не все сектора)

--- src/js/main.js
+++ src/js/main.js
@@ -26,23 +26,139 @@ if($('#datepicker1').lenth){
 }
 
 
+$(function(){
 
-$('.mesto-table table td.active').click(function(){
-	$(this).toggleClass('zakaz')
-})
+	$('.mesto-table table td.active').click(function(){
+		$(this).toggleClass('zakaz')
+	})
 
-$('.led-right button').click(function(){
-	$(".bilet-modal").fadeIn();
-})
+	$('.led-right button').click(function(){
+		$(".bilet-modal").fadeIn();
+	})
 
-$('.modal-close').click(function(){
-	$(this).closest('.bilet-modal').fadeOut()
-})
+	$('.modal-close').click(function(){
+		$(this).closest('.bilet-modal').fadeOut()
+	})
 
-$(".bilet-modal").mouseup(function (e){ // событие клика по веб-документу
-	var div = $(".modal-wrap"); // тут указываем ID элемента
-	if (!div.is(e.target) // если клик был не по нашему блоку
-		&& div.has(e.target).length === 0) { // и не по его дочерним элементам
-			$(this).closest('.bilet-modal').fadeOut()
+	$(".bilet-modal").mouseup(function (e){ // событие клика по веб-документу
+		var div = $(".modal-wrap"); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+			&& div.has(e.target).length === 0) { // и не по его дочерним элементам
+				$(this).closest('.bilet-modal').fadeOut()
+		}
+	});
+
+
+	//выбор сектора
+	$('.activepath').click(function(){
+		$(this).toggleClass('active')
+	})
+
+	var x;
+	var y;
+	var ticker = 0;
+	let dataId;
+	let dataPrice;
+	let dataMest;
+	let dataFoto;
+	let dataLink;
+	$('.sector-schema-wrap').mousemove(function(event){
+		
+		var pos = $('.sector-schema-wrap').offset();
+		var elem_left = pos.left.toFixed(0);
+		var elem_top = pos.top.toFixed(0);
+		x = event.pageX - elem_left;
+		y = event.pageY - elem_top;
+	});
+
+
+	function modalrender(){
+		$('.smodal').fadeIn(200)
+
+		$('#sector-num').html(dataId)
+		$('#smodal-mest').html(dataMest)
+		$('#smodal-price').html(dataPrice)
+		$('#smodal-foto').attr('src', dataFoto)
+		$('#smodal-link').attr('href', dataLink)
+
+		$('.sector-schema-wrap').click()
+		$('.smodal').css('top', y)
+		$('.smodal').css('left', x)	
 	}
-});
+	
+	$(".activepath").mouseenter(function(e){
+		$(this).addClass('pathfocus')
+
+		dataId = $(this).attr('data-id')
+		dataPrice = $(this).attr('data-price')
+		dataMest = $(this).attr('data-mest')
+		dataFoto = $(this).attr('data-foto')
+		dataLink = $(this).attr('data-link')
+
+		var myVar = setInterval(function(e){
+			if($(".activepath").hasClass('pathfocus') != 0){
+				ticker++;
+				if(ticker == 15){
+					clearInterval(myVar);
+					ticker = 0
+
+					modalrender()
+				}
+			}
+		}, 100)
+		
+	});
+
+	$(".activepath").mouseout(function(){
+		$(".activepath").removeClass('pathfocus')
+		ticker = 0
+	})
+
+	$(document).mouseup(function (e){ // событие клика по веб-документу
+		var div = $('.smodal'); // тут указываем ID элемента
+		if (!div.is(e.target) // если клик был не по нашему блоку
+		    && div.has(e.target).length === 0) { // и не по его дочерним элементам
+			$('.smodal').fadeOut()
+		}
+	});
+	
+	function modalReload(){
+		$('#sector-num').html(dataId)
+		$('#smodal-mest').html(dataMest)
+		$('#smodal-price').html(dataPrice)
+		$('#smodal-foto').attr('src', dataFoto)
+		$('#smodal-link').attr('href', dataLink)
+		
+	}
+	$('.smodal-right-btn').click(function(){
+		let nextPath = parseInt(dataId) + 1
+		if(nextPath <= 413){
+			let $nextPath = $('.activepath[data-id=' + nextPath + ']')
+			
+			dataId = $nextPath.attr('data-id')
+			dataPrice = $nextPath.attr('data-price')
+			dataMest = $nextPath.attr('data-mest')
+			dataFoto = $nextPath.attr('data-foto')
+			dataLink = $nextPath.attr('data-link')
+
+			modalReload()
+		}
+	})
+
+	$('.smodal-left-btn').click(function(){
+		let prevPath = parseInt(dataId) - 1
+		
+		if(prevPath >= 400){
+			let $nextPath = $('.activepath[data-id=' + prevPath + ']')
+
+			dataId = $nextPath.attr('data-id')
+			dataPrice = $nextPath.attr('data-price')
+			dataMest = $nextPath.attr('data-mest')
+			dataFoto = $nextPath.attr('data-foto')
+			dataLink = $nextPath.attr('data-link')
+
+			modalReload()
+		}
+		
+	})
+})
